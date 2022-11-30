fdfe1acafb1ec2f787f8657f157fb623e215fba3
jjangjjh2222 <jjangjjh2222@naver.com>
2022-11-30 19:53:24 +0900

    first commit

---
+++ src/main/java/toonlog/toonlogspring/ToonlogSpringApplication.java
@@ -0,0 +1,13 @@
+package toonlog.toonlogspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ToonlogSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ToonlogSpringApplication.class, args);
+	}
+
+}
---
+++ src/test/java/toonlog/toonlogspring/ToonlogSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package toonlog.toonlogspring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ToonlogSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
